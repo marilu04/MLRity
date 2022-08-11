c9fc17bbe875935a2c83dd8e41e0024fc7022ac2
marilu04 <mlnl1@aluno.ifal.edu.br>
2022-08-11 09:12:32 -0300

    frases

---
+++ MLRity/templates/static/js/script.js
@@ -0,0 +1,6 @@
+const frases = ['Se nao puder fazer tudo, faca tudo o que puder.',
+'Viva sua vida como se fosse a ultima.', 'O amanha sempre chega.']
+
+function Random(){
+    document.getElementById("aleatorio") = frases.random();
+}
