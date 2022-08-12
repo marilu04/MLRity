b65ddd7ebaad1f7761e7efd50ff75b25234b570d
renata <rbfs1@aluno.ifal.edu.br>
2022-08-12 12:05:06 -0300

    mudancas

--- MLRity/templates/static/js/script.js
+++
@@ -1,6 +0,0 @@
-const frases = ['Se nao puder fazer tudo, faca tudo o que puder.',
-'Viva sua vida como se fosse a ultima.', 'O amanha sempre chega.']
-
-function Random(){
-    document.getElementById("aleatorio") = frases.random();
-}
